f1086575f4fb322c0eba8fb700f98d44a0e72e93
System <none>
2017-04-13 12:26:50 +0000

    🏑💔 Updated with Glitch

--- server.js
+++ server.js
@@ -34,11 +34,15 @@ app.post("/mattermost/slashcommand", (req, res) => {
     gifUrl => res.send({
       response_type: "in_channel",
       text: `/gif ${req.body.text} \n ![embedded image](${gifUrl})`,
-      // the following params are disabled by admins right now 
       username: req.body.user_name,
-      icon_url: `https://matter.mercedes-benz.com/api/v3/users/${req.body.user_id}/image`
+      icon_url: `https://matter.mercedes-benz.com/api/v3/users/${req.body.user_id}/image`,
     }),
-    () => res.send({ text: notFoundMsg, response_type: "ephemeral"  }),
+    () => res.send({ 
+      text: notFoundMsg, 
+      response_type: "ephemeral", 
+      username: 'gif-hook', 
+      icon_url: 'https://media.giphy.com/avatars/originals/jbZknXnoVxIt.jpg'  
+    }),
     error => res.send(500)
   )
 })
